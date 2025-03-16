a086c0256b9bacbb6a39153f0622a159d285546c
Sahithyan <sahithyan2701@gmail.com>
2025-03-16 23:56:13 +0530

    minor fixes

--- src/main.c
+++ src/main.c
@@ -8,24 +8,23 @@
 int LOG_LEVEL = LOG_NORMAL;
 
 int main(const int argc, char **argv) {
-  if (argc == 1) {
-    return subcmd_serve();
-  }
-  if (strcmp(argv[1], "--verbose") == 0) {
-    LOG_LEVEL = LOG_VERBOSE;
-    return subcmd_serve();
-  }
-
-  if (strcmp("help", argv[1]) == 0) {
-    subcmd_help();
-    return 0;
-  }
-  if (strcmp("version", argv[1]) == 0) {
-    subcmd_version();
-    return 0;
+  if (argc > 1) {
+    if (strcmp("help", argv[1]) == 0) {
+      subcmd_help();
+      return 0;
+    }
+    if (strcmp("version", argv[1]) == 0) {
+      subcmd_version();
+      return 0;
+    }
   }
 
-  printf("Unknown command: %s\n", argv[1]);
-  puts("Run `cerve help` for usage information\n");
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "--verbose") == 0) {
+      LOG_LEVEL = LOG_VERBOSE;
+    }
+  }
+
+  subcmd_serve();
   return 0;
 }
--- src/subcmds/help.c
+++ src/subcmds/help.c
@@ -3,10 +3,10 @@
 void subcmd_help() {
   subcmd_version();
 
-  puts("Serve with C\n\n");
-  puts("When run without any commands, a simple HTTP server will be started.\n");
-  puts("Commands\n");
-  puts("    help    - prints this message\n");
-  puts("    version - prints the current version\n");
+  puts("Serve with C\n");
+  puts("When run without any commands, a simple HTTP server will be started.");
+  puts("Commands");
+  puts("    help    - prints this message");
+  puts("    version - prints the current version");
 }
 
